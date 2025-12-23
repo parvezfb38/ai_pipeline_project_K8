fde5b87f327ab08bf577de5f5dbd84774c027f93
ashiiikhan <ayeshakhanzaib4@gmail.com>
2025-12-23 13:04:51 -0800

    Initial commit

---
+++ k6/prod/test1.js
@@ -0,0 +1,17 @@
+import http from "k6/http";
+import { check, sleep } from "k6";
+
+export let options = {
+  thresholds: {
+    http_req_duration: ["p(95)<500"],
+    http_req_failed: ["rate<0.01"]
+  }
+};
+
+export default function () {
+  const res = http.get("https://test.k6.io");
+  check(res, {
+    "status is 200": (r) => r.status === 200
+  });
+  sleep(1);
+}
